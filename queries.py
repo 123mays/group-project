3f34ea786b49f4bdde702dbf61f0bef1509054fc
Mays Neiroukh <136735815+123mays@users.noreply.github.com>
2024-06-01 14:46:29 -0500

    Update queries.py

--- queries.py
+++ queries.py
@@ -53,5 +53,5 @@ def query_db(selected_year, selected_age, selected_sex, selected_race, selected_
     return data
 
 # Example usage
- data = query_db("2020", "Age", "Sex", "Race/Ethnicity", "Grade", "Location", "Topic")
+ data = query_db("2020", "18 - 44y", "Male", "White", "Grade", "Idaho", "Cancer")
  print(data)
