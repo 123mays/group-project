e3ed5905561c57a75192a09f3378b29b9fdb58e4
Mays Neiroukh <136735815+123mays@users.noreply.github.com>
2024-06-01 19:56:26 -0500

    Update queriesbystratification.py fixed issue when running the file

--- queriesbystratification.py
+++ queriesbystratification.py
@@ -1,67 +1,86 @@
 import psycopg2
 
-
 def query_db():
     conn = psycopg2.connect(
         host="localhost",
         port=5432,
         database="akeelh",
         user="akeelh",
-        password="spring482farm")
+        password="spring482farm"
+    )
     cur = conn.cursor()
 
+    # Queries for 2018 table
+    cur.execute("select stratificationcategory1 from eighteentable where stratificationcategory1 = 'Overall'")
+    result = cur.fetchone()
+    print(result)
+    cur.execute("select stratificationcategory1 from eighteentable where stratificationcategory1 = 'Age'")
+    result = cur.fetchone()
+    print(result)
+    cur.execute("select stratificationcategory1 from eighteentable where stratificationcategory1 = 'Sex'")
+    result = cur.fetchone()
+    print(result)
+    cur.execute("select stratificationcategory1 from eighteentable where stratificationcategory1 = 'Race/Ethnicity'")
+    result = cur.fetchone()
+    print(result)
 
+    # Queries for 2019 table
+    cur.execute("select stratificationcategory1 from nineteentable where stratificationcategory1 = 'Overall'")
+    result = cur.fetchone()
+    print(result)
+    cur.execute("select stratificationcategory1 from nineteentable where stratificationcategory1 = 'Age'")
+    result = cur.fetchone()
+    print(result)
+    cur.execute("select stratificationcategory1 from nineteentable where stratificationcategory1 = 'Sex'")
+    result = cur.fetchone()
+    print(result)
+    cur.execute("select stratificationcategory1 from nineteentable where stratificationcategory1 = 'Race/Ethnicity'")
+    result = cur.fetchone()
+    print(result)
 
-# Queries for 2018 table
-	cur.execute("select stratificationcategory1 from eighteentable where tratificationategory1 = 'Overall'")
-	result = cur.fetchone()
-	cur.execute("select stratificationcategory1 from eighteentable where tratificationategory1 = 'Age'")
-	result = cur.fetchone()
-	cur.execute("select stratificationcategory1 from eighteentable where tratificationategory1 = 'Sex'")
-	result = cur.fetchone()
-	cur.execute("select stratificationcategory1 from eighteentable where tratificationategory1 = 'Race/Ethnicity'")
-	result = cur.fetchone()
+    # Queries for 2020 table
+    cur.execute("select stratificationcategory1 from twentytable where stratificationcategory1 = 'Overall'")
+    result = cur.fetchone()
+    print(result)
+    cur.execute("select stratificationcategory1 from twentytable where stratificationcategory1 = 'Age'")
+    result = cur.fetchone()
+    print(result)
+    cur.execute("select stratificationcategory1 from twentytable where stratificationcategory1 = 'Sex'")
+    result = cur.fetchone()
+    print(result)
+    cur.execute("select stratificationcategory1 from twentytable where stratificationcategory1 = 'Race/Ethnicity'")
+    result = cur.fetchone()
+    print(result)
 
-# Queries for 2019 table
-	cur.execute("select stratificationcategory1 from nineteentable where tratificationategory1 = 'Overall'")
-	result = cur.fetchone()
-	cur.execute("select stratificationcategory1 from nineteentable where tratificationategory1 = 'Age'")
-	result = cur.fetchone()
-	cur.execute("select stratificationcategory1 from nineteentable where tratificationategory1 = 'Sex'")
-	result = cur.fetchone()
-	cur.execute("select stratificationcategory1 from eighteentable where tratificationategory1 = 'Race/Ethnicity'")
-	result = cur.fetchone()
+    # Queries for 2021 table
+    cur.execute("select stratificationcategory1 from twentyonetable where stratificationcategory1 = 'Overall'")
+    result = cur.fetchone()
+    print(result)
+    cur.execute("select stratificationcategory1 from twentyonetable where stratificationcategory1 = 'Age'")
+    result = cur.fetchone()
+    print(result)
+    cur.execute("select stratificationcategory1 from twentyonetable where stratificationcategory1 = 'Sex'")
+    result = cur.fetchone()
+    print(result)
+    cur.execute("select stratificationcategory1 from twentyonetable where stratificationcategory1 = 'Race/Ethnicity'")
+    result = cur.fetchone()
+    print(result)
 
-# Queries for 2020 table
-	cur.execute("select stratificationcategory1 from twentytable where tratificationategory1 = 'Overall'")
-	result = cur.fetchone()
-	cur.execute("select stratificationcategory1 from twentytable where tratificationategory1 = 'Age'")
-	result = cur.fetchone()
-	cur.execute("select stratificationcategory1 from twentytable where tratificationategory1 = 'Sex'")
-	result = cur.fetchone()
-	cur.execute("select stratificationcategory1 from twentytable where tratificationategory1 = 'Race/Ethnicity'")
-	result = cur.fetchone()
+    # Queries for 2022 table
+    cur.execute("select stratificationcategory1 from twentytwotable where stratificationcategory1 = 'Overall'")
+    result = cur.fetchone()
+    print(result)
+    cur.execute("select stratificationcategory1 from twentytwotable where stratificationcategory1 = 'Age'")
+    result = cur.fetchone()
+    print(result)
+    cur.execute("select stratificationcategory1 from twentytwotable where stratificationcategory1 = 'Sex'")
+    result = cur.fetchone()
+    print(result)
+    cur.execute("select stratificationcategory1 from twentytwotable where stratificationcategory1 = 'Race/Ethnicity'")
+    result = cur.fetchone()
+    print(result)
 
-# Queries for 2021 table
-	cur.execute("select stratificationcategory1 from twentyonetable where tratificationategory1 = 'Overall'")
-	result = cur.fetchone()
-	cur.execute("select stratificationcategory1 from twentyonetable where tratificationategory1 = 'Age'")
-	result = cur.fetchone()
-	cur.execute("select stratificationcategory1 from twentyonetable where tratificationategory1 = 'Sex'")
-	result = cur.fetchone()
-	cur.execute("select stratificationcategory1 from twentyonetable where tratificationategory1 = 'Race/Ethnicity'")
-	result = cur.fetchone()
-
-# Queries for 2022 table
-	cur.execute("select stratificationcategory1 from twentytwotable where tratificationategory1 = 'Overall'")
-	result = cur.fetchone()
-	cur.execute("select stratificationcategory1 from twentytwotable where tratificationategory1 = 'Age'")
-	result = cur.fetchone()
-	cur.execute("select stratificationcategory1 from twentytwotable where tratificationategory1 = 'Sex'")
-	result = cur.fetchone()
-	cur.execute("select stratificationcategory1 from twentytwotable where tratificationategory1 = 'Race/Ethnicity'")
-	result = cur.fetchone()
-	cur.close()
-	conn.close()
+    cur.close()
+    conn.close()
 
 query_db()
