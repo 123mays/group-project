150131cd6b0fca345b97b5301f1fccdba1be7154
Mays Neiroukh <136735815+123mays@users.noreply.github.com>
2024-06-03 14:42:46 -0500

    Update homepage.py

--- homepage.py
+++ homepage.py
@@ -1,8 +1,9 @@
 '''
-Authors: Mays Neiroukh, Kellen Knop
 This is a Flask web application that connects to a PostgreSQL database 
 to retrieve and display data based on user-selected filters.
 It provides dropdown options for filtering data and displays results based on these selections.
+
+Authors: Mays Neiroukh, Kellen Knop
 '''
 from flask import Flask, render_template, request
 import psycopg2
